6c4906d2960d86c8a3394c2eca2b9b176a62fd69
WilmerAraya <wilmeraraya21@gmail.com>
2024-02-21 14:52:50 -0600

    Add James Bond Game project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -6,34 +6,14 @@ import { MdWork } from "react-icons/md";
 export function Project({ project }) {
   return (
     <div>
-      {project.link !== "" ? (
-        <a
-          href={project.link}
-          className="text-yellow-300 text-lg font-semibold cursor-pointer hover:text-yellow-100"
-          target="_blank"
-        >
-          {project.title}
-        </a>
-      ) : (
-        <h1 className="text-gray-200 text-lg font-semibold">{project.title}</h1>
-      )}
+      {project.title}
 
       <p className="text-gray-300">{project.description}</p>
 
-      {project.company !== "" && (
-        <a
-          href={project.companyLink}
-          className="text-white my-2 flex items-center hover:text-gray-400"
-          target="_blank"
-        >
-          <MdWork className="mr-2" />
-          {project.company}
-        </a>
-      )}
       {project.github !== "" && (
         <a
           href={project.github}
-          className="text-white flex items-center my-2 hover:text-gray-400"
+          className="text-white flex items-center my-2 hover:text-yellow-300 underline"
           target="_blank"
         >
           <FaSquareGithub className="mr-2" />
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,12 +5,33 @@ import { Project } from "./Components/Project";
 
 const PROJECTS = [
   {
-    title: "Finances Web Application",
-    description:
-      "A web application for the company Lago & Sarmiento, an organization that offers financial mentoring. This web application is for financial projections targeting SMEs, it was built using .NET framework and a Microsoft SQL Server database.",
-    company: "Lago & Sarmiento",
-    companyLink: "https://www.lagoysarmiento.com/",
-    link: "",
+    title: (
+      <a
+        href=""
+        className="text-yellow-300 text-lg font-semibold cursor-default"
+        target="_blank"
+      >
+        Finances Web Application
+      </a>
+    ),
+    description: (
+      <p>
+        A web application for the company{" "}
+        <a
+          className="underline hover:text-yellow-300"
+          href="https://www.lagoysarmiento.com/"
+          target="_blank"
+        >
+          Lago & Sarmiento
+        </a>
+        , an organization that offers financial mentoring. The purpose of the
+        application is to generate financial projections for SMEs, enabling
+        users to manage their business finances effectively. It was built using
+        the <b>.NET Framework</b>, with <b>Entity Framework</b> for seamless
+        data access, <b>Blazor</b> for the user interface, and a{" "}
+        <b>Microsoft SQL Server</b> database.
+      </p>
+    ),
     images: [
       "https://i.gyazo.com/08f465383d80a3ad4d9e67c84c4cc1eb.png",
       "https://i.gyazo.com/64e68932ff9540a9791be3b4c8f8e0bf.png",
@@ -20,33 +41,108 @@ const PROJECTS = [
       "https://github.com/Wumpus-Proyecto-PI/CI0128_22B_Wumpus/tree/Sprint03",
   },
   {
-    title: "RIDE Dashboard",
-    description:
-      "I participated in the development of the RIDE dashboard, an online food delivery platform that connects users with local restaurants. Implemented with Angular, Typescript and Firebase.",
-    company: "RIDE Mensajería Ecológica",
-    companyLink:
-      "https://sites.google.com/view/ridemensajeriaecologica/inicio?authuser=0",
-    link: "",
+    title: (
+      <a
+        href="https://dashboard-somosridecr.web.app/login"
+        className="text-yellow-300 text-lg font-semibold underline hover:text-yellow-200"
+        target="_blank"
+      >
+        RIDE Dashboard
+      </a>
+    ),
+    description: (
+      <p>
+        As part of my professional practice, I contributed to the development of
+        a web application for{" "}
+        <a
+          className="underline hover:text-yellow-300"
+          href="https://sites.google.com/view/ridemensajeriaecologica/inicio?authuser=0"
+          target="_blank"
+        >
+          RIDE
+        </a>
+        , a company that specializes in eco-friendly deliveries using bicycles.
+        The developed web application aims to enable RIDE employees to manage
+        customer orders and assign cyclists for delivering orders to their
+        respective locations. It was developed using <b>Angular</b>,{" "}
+        <b>Typescript</b>, and <b>Firebase</b>.
+      </p>
+    ),
     images: [
       "https://github.com/WilmerAraya/WilmerAraya/blob/WilmerAraya-assets/images/ride-dashboard.png?raw=true",
       "https://i.gyazo.com/b136d647cb8ae11298003db2cb1e4b1c.png",
     ],
-    github: "",
+    github: "https://github.com/gatitolabs/ride-dbtypes",
   },
   {
-    title: "De Feria CR",
-    description:
-      "I participated in the development of De Feria, a web application facilitating the location of farmer's markets throughout Costa Rica. The project, built in Python with the Django framework and backed by a PostgreSQL database, focused on providing users with an intuitive and efficient experience.",
-    company: "",
-    companyLink: "",
-    link: "https://deferia.cr/",
+    title: (
+      <a
+        href="https://deferia.cr/"
+        className="text-yellow-300 text-lg font-semibold underline hover:text-yellow-200"
+        target="_blank"
+      >
+        De Feria CR
+      </a>
+    ),
+    description: (
+      <p>
+        As part of the University Community Work{" "}
+        <a
+          className="underline hover:text-yellow-300"
+          href="https://eie.ucr.ac.cr/proyectos/TC-691"
+          target="_blank"
+        >
+          TC-691
+        </a>
+        , I participated in the development of De Feria, a web application
+        facilitating the location of farmer's markets throughout Costa Rica. The
+        project, built in <b>Python</b> with the <b>Django Framework</b> and
+        backed by a <b>PostgreSQL</b> database, focused on providing users with
+        an intuitive and efficient experience.
+      </p>
+    ),
     images: [
       "https://i.gyazo.com/a1c80c9c43a3cf4c3a5d82848d202547.png",
       "https://i.gyazo.com/f5eec304f5eaf74f10b9186e3c6fc6f9.png",
       "https://i.gyazo.com/65a90954acf4829350f675bb3e907d4b.png",
       "https://i.gyazo.com/ef2d4a52bbcb4a6c72fc5ace0461add1.png",
     ],
-    github: "",
+    github: "https://github.com/fabianabarca/ferias",
+  },
+  {
+    title: (
+      <a
+        href=""
+        className="text-yellow-300 text-lg font-semibold cursor-default"
+        target="_blank"
+      >
+        James Bond Game
+      </a>
+    ),
+    description: (
+      <p>
+        Game for Windows, based on the rules of the card game{" "}
+        <a
+          className="underline hover:text-yellow-300"
+          href="https://en.wikipedia.org/wiki/James_Bond_(card_game)"
+          target="_blank"
+        >
+          James Bond
+        </a>
+        , developed in <b>Java</b> using <b>JavaFX</b>. The objective of this
+        project was to investigate and apply both <b>SOLID principles</b> and
+        programming <b>design patterns</b> such as the <b>template method</b>,
+        so that the result could also serve as a framework to be used for
+        creating similar board games.
+      </p>
+    ),
+    images: [
+      "https://i.gyazo.com/92af7736d6923a2762214f4241d9d22b.jpg",
+      "https://i.gyazo.com/a00f548805903ad8554ea4db199c62a3.png",
+      "https://i.gyazo.com/888144415ac44941869a61300954bc12.jpg",
+      "https://i.gyazo.com/eb6fcf8d4488ce74a2cc9fe47f4f368a.png",
+    ],
+    github: "https://github.com/FabianOrozcoChaves/JamesBond",
   },
 ];
 
